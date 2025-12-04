fbc7dbe27be05d886e06e5bf5b7495762ecf8b6b
Johnny Africa <johnnyafrica211@gmail.com>
2025-12-04 11:29:24 +0000

    Ensure publicDir copies _redirects

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'),
     },
   },
+  publicDir: path.resolve(__dirname, 'public'), // ensures _redirects is copied
 })
-
-
